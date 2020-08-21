469fd8a6286478d7af4114f1190852e607178a13
KassenBoyaubay <KassenBoyaubay@nu.edu.kz>
2020-08-21 20:27:51 +0600

    added dark_light_toggle_mode

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import './App.css'
-import AppStyledComponent from './AppStyledComponent'
+import "./App.css";
+import AppStyledComponent from "./AppStyledComponent";
 
-import Tweets from './components/Tweets'
-import State from './components/State'
-import ButtonNeon from './components/button_neon_light/buttonNeon'
-import ClockDigital from './components/clock_digital/clockDigital'
-import ClockNeumorphism from './components/clock_neumorphism/clockNeumorphism'
+import Tweets from "./components/Tweets";
+import State from "./components/State";
+import ButtonNeon from "./components/button_neon_light/buttonNeon";
+import ClockDigital from "./components/clock_digital/clockDigital";
+import ClockNeumorphism from "./components/clock_neumorphism/clockNeumorphism";
+import ToggleMode from "./components/dark_light_toggle_mode/toggleMode";
 
 const App = () => {
-  const name = 'Kas'
-  const age = 26
+  const name = "Kas";
+  const age = 26;
 
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     // Fake Back-end API
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => {
-        console.log(res.data)
-        setTodos(res.data)
-      })
-  }, [])
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => {
+        console.log(res.data);
+        setTodos(res.data);
+      });
+  }, []);
 
   /*
     // Post to server (Add todo)
@@ -42,31 +44,37 @@ const App = () => {
   */
 
   const buttonNeonContainer = {
-    margin: '0',
-    padding: '0',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '10vh',
-    background: '#031321',
-    fontFamily: 'consolas'
-  }
+    margin: "0",
+    padding: "0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "10vh",
+    background: "#031321",
+    fontFamily: "consolas",
+  };
 
   const clockNeumorphismContainer = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '40vh',
-    background: '#091921'
-  }
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "40vh",
+    background: "#091921",
+  };
 
   return (
     <div className="App">
-      <h1>Hell<span><i className="fa fa-spinner fa-spin"></i></span> React </h1>
+      <h1>
+        Hell
+        <span>
+          <i className="fa fa-spinner fa-spin"></i>
+        </span>{" "}
+        React{" "}
+      </h1>
       <br />
       <h2>Neon Button Light</h2>
       <div style={buttonNeonContainer}>
-        <ButtonNeon name={'Neon Button'} />
+        <ButtonNeon name={"Neon Button"} />
       </div>
       <br />
       <h2>Digital Clock</h2>
@@ -77,32 +85,57 @@ const App = () => {
         <ClockNeumorphism />
         <br />
       </div>
-      <div className="home" style={{ display: 'flex' }}>
+      <br />
+      <h2>Dark Light Toggle Mode</h2>
+      <ToggleMode />
+      <div className="home" style={{ display: "flex" }}>
         <Tweets me={name} age={age} />
-        <Tweets me='NotKas' age='30' />
+        <Tweets me="NotKas" age="30" />
       </div>
-      <div style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column' }}>
+      <div
+        style={{
+          backgroundColor: "black",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <State />
         <br />
-        <Link to="/animatedWebsite"><button>Animated Website</button></Link>
-        <Link to="/bootstrapTemplate"><button>Bootstrap Template</button></Link>
-        <Link to="/bootstrapPortfolio"><button>Bootstrap Portfolio</button></Link>
-        <Link to="/contactForm"><button>Contact Form</button></Link>
-        <a href="./Blogger/Index.html"><button>Blogger</button></a>
-        <a href="./Books_bootstrap/src/Index.html"><button>Books bootstrap</button></a>
+        <Link to="/animatedWebsite">
+          <button>Animated Website</button>
+        </Link>
+        <Link to="/bootstrapTemplate">
+          <button>Bootstrap Template</button>
+        </Link>
+        <Link to="/bootstrapPortfolio">
+          <button>Bootstrap Portfolio</button>
+        </Link>
+        <Link to="/contactForm">
+          <button>Contact Form</button>
+        </Link>
+        <a href="./Blogger/Index.html">
+          <button>Blogger</button>
+        </a>
+        <a href="./Books_bootstrap/src/Index.html">
+          <button>Books bootstrap</button>
+        </a>
       </div>
       <AppStyledComponent color="blue" size="large">
         Blue
-      </AppStyledComponent >
+      </AppStyledComponent>
       <AppStyledComponent color="green" size="small" noBorder>
         Green
-      </AppStyledComponent >
+      </AppStyledComponent>
       <div className="todos">
         <h2>ToDo API</h2>
-        {todos.map(todo => <p key={todo.id}>{todo.id}. {todo.title}</p>)}
+        {todos.map((todo) => (
+          <p key={todo.id}>
+            {todo.id}. {todo.title}
+          </p>
+        ))}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/dark_light_toggle_mode/toggleMode.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./toggleMode.scss";
+
+const ToggleMode = () => {
+  const [toggleClass, setToggleClass] = useState("");
+  const [dataTheme, setDataTheme] = useState("");
+  const [checked, setChecked] = useState(true);
+
+  const checkbox = () => {
+    setChecked(!checked);
+    if (checked) {
+      trans();
+      setDataTheme("dark");
+    } else {
+      trans();
+      setDataTheme("light");
+    }
+  };
+
+  let trans = () => {
+    setToggleClass("transition");
+    window.setTimeout(() => {
+      setToggleClass("");
+    }, 1000);
+  };
+
+  return (
+    <div className={`ToggleMode ${toggleClass}`} data-theme={dataTheme}>
+      <div className="container">
+        <h1>Light / Dark Mode</h1>
+        <div className="toggle-container">
+          <input
+            type="checkbox"
+            id="switch"
+            name="theme"
+            checked={checked}
+            onChange={() => checkbox()}
+          />
+          <label for="switch">Toggle</label>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          magni optio impedit cum beatae fugit, quis enim eligendi cumque
+          expedita pariatur dicta assumenda delectus quia voluptatum eos quo
+          quam vitae.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleMode;
