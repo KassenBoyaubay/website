199d2c55ad7fff3ecacfa89dbc2e321c3f4e55c5
KassenBoyaubay <KassenBoyaubay@nu.edu.kz>
2020-08-31 00:01:26 +0600

    added fully book_list

--- src/components/book_list/bookList.jsx
+++ src/components/book_list/bookList.jsx
@@ -1,4 +1,36 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
 
 const BookList = () => {
   const [book, setBook] = useState({
@@ -7,6 +39,23 @@ const BookList = () => {
     isbn: "",
   });
   const [allBooks, setAllBooks] = useState([]);
+  const [alert, setAlert] = useState();
+
+  const showAlert = (message, className) => {
+    setAlert(
+      <div className={`text-center alert alert-${className}`}>{message}</div>
+    );
+  };
+
+  useEffect(() => {
+    setTimeout(() => setAlert(), 5000);
+  }, [alert]);
+
+  useEffect(() => {
+    const books = Store.getBooks();
+
+    books.forEach((b) => setAllBooks((prev) => [...prev, b]));
+  }, []);
 
   const submit = (event) => {
     event.preventDefault();
@@ -17,6 +66,10 @@ const BookList = () => {
         author: "",
         isbn: "",
       });
+      Store.addBook(book);
+      showAlert("Book Added", "success");
+    } else {
+      showAlert("Please fill in all fields", "danger");
     }
   };
 
@@ -28,9 +81,11 @@ const BookList = () => {
     }));
   };
 
-  const deleteBook = (event, id) => {
+  const deleteBook = (event, id, isbn) => {
     event.preventDefault();
     setAllBooks(allBooks.filter((item, i) => i !== id));
+    Store.removeBook(isbn);
+    showAlert("Book Removed", "success");
   };
 
   return (
@@ -40,7 +95,12 @@ const BookList = () => {
           <i className="fas fa-book-open text-primary"></i> My
           <span className="text-primary">Book</span>List
         </h1>
-        <form id="book-form" onSubmit={(e) => submit(e)}>
+        {alert}
+        <form
+          style={alert ? {} : { marginTop: "74px" }}
+          id="book-form"
+          onSubmit={(e) => submit(e)}
+        >
           <div className="form-group">
             <label htmlFor="title">Title</label>
             <input
@@ -119,7 +179,7 @@ const Book = (props) => {
         <a
           href="#"
           className="btn btn-danger btn-sm delete"
-          onClick={(e) => props.deleteBook(e, props.id)}
+          onClick={(e) => props.deleteBook(e, props.id, props.isbn)}
         >
           X
         </a>
